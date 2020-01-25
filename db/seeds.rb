697a21a868ea61932e5fcea407d52579e6b764c9
Rails Udemy Course <railsudemycourse@gmail.com>
2020-01-25 00:12:50 +0000

    Modified chatroom index view to use messages from the db and removed hard coded messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "Evgeny", password: "password")
-User.create(username: "Jonsnow", password: "password")
-User.create(username: "Arya", password: "password")
-User.create(username: "Frodo", password: "password")
-User.create(username: "Gandalf", password: "password")
+evgeny = User.create(username: "Evgeny", password: "password")
+jon_snow = User.create(username: "Jonsnow", password: "password")
+arya = User.create(username: "Arya", password: "password")
+frodo = User.create(username: "Frodo", password: "password")
+gandalf = User.create(username: "Gandalf", password: "password")
+
+message1 = Message.new(body: "Hi, my name is evgeny")
+message1.user = evgeny
+message1.save
+
+message2 = Message.new(body: "Hi, my name is Jon Snow")
+message2.user = jon_snow
+message2.save
+
+message3 = Message.new(body: "Hi, my name is Arya")
+message3.user = arya
+message3.save
+
+message4 = Message.new(body: "Hi, my name is Frodo")
+message4.user = frodo
+message4.save
+
+message5 = Message.new(body: "Hi, my name is Gandalf")
+message5.user = gandalf
+message5.save
