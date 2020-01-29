ea7fa428f7b9771fcd18646bd3cce677b5aac5f1
Rails Udemy Course <railsudemycourse@gmail.com>
2020-01-29 20:42:36 +0000

    finished basic app functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # root route
+  
   root 'chatroom#index'
   
   # login 
+  
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  # messages
+  
+  post 'message', to: 'messages#create'
+  
+  # Action Cable
+  
+  mount ActionCable.server, at: '/cable'
+  
 end
